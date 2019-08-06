8f6c85dc58f1f9ea647312daf5eb98fada3f44ad
BVigneshwar <bvigneshwar643@gmail.com>
2019-08-06 22:38:49 +0530

    Database for notes added

---
+++ app/src/main/java/com/vignesh/remainder/NotesDatabaseHandler.java
@@ -0,0 +1,43 @@
+package com.vignesh.remainder;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NotesDatabaseHandler extends SQLiteOpenHelper {
+    Context context;
+    static String DB_name = "Notes_DB";
+    static int DB_version = 1;
+    static String table = "Notes_table";
+    static String id = "id";
+    static String title = "title";
+    static String description = "description";
+    static String category = "category";
+
+    NotesDatabaseHandler(Context context){
+        super(context, DB_name, null, DB_version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create_category_table = "CREATE TABLE category_table (id INTEGER PRIMARY KEY, name TEXT);";
+        db.execSQL(create_category_table);
+        String create_notes_table = "CREATE TABLE " + table + "("
+                + id + " INTEGER PRIMARY KEY," + title + " TEXT,"
+                + description + " TEXT," + category +" INTEGER, FOREIGN KEY("+ category +") REFERENCES category_table(id))";
+        db.execSQL(create_notes_table);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + table);
+        onCreate(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + table);
+        onCreate(db);
+    }
+}
